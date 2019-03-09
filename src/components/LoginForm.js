c7844226828c5bbe71119cc51a21138e3847a91f
PatAkil <patrickakil@hotmail.com>
2019-03-09 17:41:44 +0100

    Setting up the firebase login request handler through async actions call

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card, CardSection, InputField, Button } from './common';
-import { emailChanged, passwordChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 
 class LoginForm extends Component {
   onEmailChange(text) {
@@ -9,6 +9,11 @@ class LoginForm extends Component {
   }
   onPasswordChange(text) {
     this.props.passwordChanged(text);
+  }
+  
+  onButtonPress() {
+    const { email, password } = this.props;
+    this.props.loginUser({ email, password });
   }
 
   render() {
@@ -32,7 +37,9 @@ class LoginForm extends Component {
           />
         </CardSection>
         <CardSection>
-          <Button>
+          <Button
+          onPress={this.onButtonPress.bind(this)}
+          >
             Login
           </Button>
         </CardSection>
@@ -49,4 +56,4 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
+export default connect(mapStateToProps, { emailChanged, passwordChanged, loginUser })(LoginForm);
